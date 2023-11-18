efe69b17a91c7452e3e8f1703eba438aed6ce51f
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-11-18 21:36:19 +0100

    working on skill slider

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -15,14 +15,14 @@ const Experience = () => {
       <article className='px-3 pb-[1rem] pt-[5rem] flex justify-between lg:items-center gap-5 lg:flex-row flex-col lg:py-0 lg:px-0 md:w-[80%] mx-auto lg:w-full'>
         <div className='w-full lg:w-[30%] flex flex-col gap-3'>
           <h2
-            className={`${poppins.className} text-[3.5rem] lg:text-[4rem] font-semibold`}
+            className={`${poppins.className} text-[3.5rem] lg:text-[4rem] font-semibold text-white`}
           >
             Over the years,
           </h2>
           {experience_Data.map((data) => (
             <p
               key={data.id}
-              className={`${poppins.className} font-semibold`}
+              className={`${poppins.className} font-semibold text-white`}
             >
               {data.para}
             </p>
