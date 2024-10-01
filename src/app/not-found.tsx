bae21cadfd3db6ec35ad27f6bb5c9b7d3e68c76d
ilesanmi erioluwa victor <ilesanmierioluwavictor@gmail.com>
2024-10-01 23:08:41 +0100

    working on 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,7 +12,6 @@ export default function NotFound() {
     const handle = handleRef.current;
 
     if (!copyContainer || !handle) return;
-
     const text = copyContainer.querySelector("p")?.innerText || "";
     const characters = text
       .split("")
@@ -25,37 +24,32 @@ export default function NotFound() {
       copyContainer.querySelector("p")!.innerHTML = characters;
     }
 
-    const splitTextTimeline = gsap.timeline();
-    const handleTL = gsap.timeline();
     const copyWidth = copyContainer.querySelector("p")?.offsetWidth || 0;
+    const charElements = gsap.utils.toArray(".char");
 
     const animateCopy = () => {
       gsap.fromTo(
-        ".char",
+        charElements,
         { autoAlpha: 0 },
         {
           autoAlpha: 1,
           stagger: 0.05,
           ease: Back.easeInOut.config(1.7),
-          onComplete: animateHandle,
+          onUpdate: function () {
+            const progress = this.progress();
+            const handleProgress = progress * copyWidth;
+            gsap.set(handle, { x: handleProgress });
+          },
         }
       );
     };
 
     const blinkHandle = () => {
-      handleTL.fromTo(
+      gsap.fromTo(
         handle,
         { autoAlpha: 0 },
         { autoAlpha: 1, duration: 0.4, repeat: -1, yoyo: true }
       );
-    };
-
-    const animateHandle = () => {
-      handleTL.to(handle, {
-        x: copyWidth,
-        duration: 0.7,
-        ease: SteppedEase.config(12),
-      });
     };
 
     gsap.delayedCall(0.2, () => {
@@ -64,39 +58,26 @@ export default function NotFound() {
     });
 
     return () => {
-      splitTextTimeline.kill();
-      handleTL.kill();
+      gsap.killTweensOf(handle);
+      gsap.killTweensOf(".char");
     };
   }, []);
 
   return (
-    // <div className="min-h-[100vh] text-center flex items-center justify-center">
-    //   <div>
-    //
-    //     <h3 className="text-[2rem]">Ohh page not found</h3>
-    //     <p className="mb-[2rem] text-lg">
-    //       We cant seem to find the page you are looking for
-    //     </p>
-    //     <Link href="/" className="notfoundbtn">
-    //       Back Home
-    //     </Link>
-    //   </div>
-    // </div>
-
     <div className="w-full h-[100vh] relative">
       <div
         className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center"
         ref={copyContainerRef}
       >
         <p className="text-black text-[24px] tracking-[0.2px] m-0">
-          404, page not found. please go back Home
+          404, page not found. please go back Home 🛑🛑
         </p>
         <span
           className="bg-[#ffe500] w-[14px] h-[30px] absolute top-0 left-0 mt-[1px]"
           ref={handleRef}
         ></span>
         <div className="relative mt-[30px]">
-          <Link href="/" className="not_found_btn">
+          <Link href="/" className="notfoundbtn">
             Back Home
           </Link>
         </div>
